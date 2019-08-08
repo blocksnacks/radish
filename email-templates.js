caf6787b60db5aa88fd53b3f1e7725acc2485988
erikwlarsen <erik.w.larsen@gmail.com>
2019-08-08 01:30:16 -0400

    feat: emoji!

--- email-templates.js
+++ email-templates.js
@@ -8,7 +8,7 @@ Hi <b>${blockstackId}</b>,
 <b>${creator}</b> has invited you to join the user group <b>${groupName}</b>. Click <b><a href="${SITE_DOMAIN}/group-invite/${inviteId}">here</a></b> to activate your membership.
 
 Thanks,
-The Blocksnacks Team
+The Blocksnacks Team 🍫
     `;
   }
 }
